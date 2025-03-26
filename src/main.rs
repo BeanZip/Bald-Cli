ff9dd4851781b2e64dcda4633c86955dacc107a2
Ichi <78321273+BeanZip@users.noreply.github.com>
2025-03-25 23:33:20 -0400

    init  commit

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use clap::Parser;
+use std::process::Command;
+
+// Search for a file pattern and display lines that it's contained in.
+#[derive(Parser, Debug)]
+#[command(name = "cli-tool")]
+struct Cli {
+    section: String,
+    section_command: String,
+}
+
+
+
+
+fn setup(){
+    
+  //TODO: Setup nvim installation 
+   if cfg!(windows) {
+        println!("System is Windows");
+        let mut script = "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+        Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression";
+         println!("Beginning to Install Scoop.....");
+         let com = Command::new("powershell").args(["/C",script]).output().expect("Failed to execute command");
+         println!("{}", String::from_utf8_lossy(&com.stdout));
+    } else if cfg!(unix){
+        println!("System is a Unix-based OS (Linux or MacOS)");
+    } else{
+        println!("DAWG WHAT ARE YOU USING THIS ON A TOASTER!???");
+        println!("Nah bro's on a {:?} ",std::env::consts::OS);
+    }
+
+}
+fn main() {
+    let args = Cli::parse();
+
+    match args.section.as_str() {
+        "first-sec" => match args.section_command.as_str() {
+            "hello" => println!("Hello There"),
+            "goodbye" => {
+                let output = Command::new("cmd")
+                    .args(["/C", "shutdown /r /t 5 && echo time for your pc's nap time"])
+                    .output()
+                    .expect("Failed to execute command");
+                println!("{}", String::from_utf8_lossy(&output.stdout));
+            },
+            "build-my-vim" => {
+                setup();
+            },
+            _ => println!("Nothing There"),
+        },
+        _ => println!("Invalid Command Provided."),
+    }
+}
